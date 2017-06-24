e9ee8aa48d83c4030bde13d7426fa19e02165599
tongari <info@tongari-as.com>
2017-06-24 22:58:45 +0900

    sh作成

--- prod.sh
+++ prod.sh
@@ -1,3 +1,57 @@
 #!/bin/sh
-bundle exec rake assets:precompile RAILS_ENV=production
 
+errorMsg="prod処理を停止します。"
+
+#bundle exec rake assets:precompile RAILS_ENV=production
+echo "コミット文言を入力してください"
+read commitMsg
+echo "コミットメッセージ -> $commitMsg"
+echo "コミットしますか？ y(yes) or n(no)"
+read isCommit
+case $isCommit in
+  y)
+    git add -A
+    git commit -m "$commitMsg"
+    echo "コミットしました"
+    ;;
+  n)
+    echo "コミットキャンセルしました。$errorMsg"
+    exit
+    ;;
+  *)
+    echo "失敗しました。$errorMsg"
+    exit
+    ;;
+esac
+
+echo "herokuにプッシュしますか？ y(yes) or n(no)"
+read isHerokuPush
+case $isHerokuPush in
+  y)
+    git push heroku master
+    ;;
+  n)
+    echo "herokuにプッシュキャンセルしました。$errorMsg"
+    exit
+    ;;
+  *)
+    echo "失敗しました。$errorMsg"
+    exit
+    ;;
+esac
+
+echo "プッシュしますか？ y(yes) or n(no)"
+read isPush
+case $isPush in
+  y)
+    git push
+    ;;
+  n)
+    echo "プッシュキャンセルしました。"
+    exit
+    ;;
+  *)
+    echo "失敗しました。"
+    exit
+    ;;
+esac
