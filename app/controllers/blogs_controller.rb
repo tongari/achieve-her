676bb988bb0a94e4bc9201f86a3159aa5cd40742
tongari <info@tongari-as.com>
2017-06-25 23:34:07 +0900

    sendgrid relation devise

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,67 +1,68 @@
 class BlogsController < ApplicationController
-  
+
   before_action :authenticate_user!
   before_action :set_blog, only:[:edit, :update, :destroy]
-  
-  
+
+
   def index
     @blogs = Blog.all
     @users = User.all
     # render :json => @blogs
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blogs_params)
     else
       @blog = Blog.new
     end
-    
+
   end
-  
+
   def create
     @blog = Blog.new(blogs_params)
     @blog.user_id = current_user.id
     if @blog.save
       redirect_to blogs_path, notice: "ブログを作成しました！"
+      NoticeMailer.sendmail_blog(@blog).deliver
     else
       render 'new'
     end
-    
+
   end
-  
+
   def edit
   end
-  
+
   def update
     if @blog.update(blogs_params)
       redirect_to blogs_path, notice: "ブログを更新しました！"
     else
       render 'edit'
     end
-    
+
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice: "ブログを削除しました！"
   end
-  
+
   def confirm
     @blog = Blog.new(blogs_params)
     render :new if @blog.invalid?
   end
-  
+
   private
     def blogs_params
       params.require(:blog).permit(:title, :content)
     end
-    
+
     def set_blog
       @blog = Blog.find(params[:id])
       puts "--------------------"
       p @blog
       puts "--------------------"
     end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,32 @@
 Rails.application.routes.draw do
-  
+
+
+
   devise_for :users
   resources :blogs, only: [:index, :new, :create, :edit, :update ,:destroy] do
     collection do
       post :confirm
     end
   end
-  
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   # resources :blogs, only:[:index, :new, :create, :edit, :update, :destroy]
   resources :contacts, only:[:new, :create] do
     collection do
       post :confirm
     end
   end
-  
+
   root 'top#index'
-  
+
   # RoutingError
   # 上から順に評価されるので、一番下に書く
   get '*path', to: 'application#error_404'
-  
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
