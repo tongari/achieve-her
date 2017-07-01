0c5ded04025cd47fcac94ca9523666f23cfb79ab
tongari <info@tongari-as.com>
2017-07-01 15:31:32 +0900

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'thawing-coast-63336.herokuapp.com' }
+  config.action_mailer.default_url_options = { host:'thawing-coast-63336.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
@@ -89,4 +89,4 @@ Rails.application.configure do
    authentication: :plain,
    enable_starttls_auto: true
   }
-end 
+end
