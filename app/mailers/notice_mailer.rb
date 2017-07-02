1356b9867226e5b875759803f410a6908a69dc72
tongari <info@tongari-as.com>
2017-07-02 14:01:08 +0900

    update

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -14,7 +14,7 @@ class NoticeMailer < ApplicationMailer
 
   def sendmail_contact(contact)
     @contact = contact
-    
+
     mail to: @contact[:email],
     subject: '【Achieve】お問い合わせありがとうございます'
   end
