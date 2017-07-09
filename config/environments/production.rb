737f77fd9d16fe620397f1e11859fdc487bd34eb
tongari <info@tongari-as.com>
2017-07-09 12:25:48 +0900

    set sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host:'thawing-coast-63336.herokuapp.com' }
+  config.action_mailer.default_url_options = { host:'calm-lowlands-28737.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
